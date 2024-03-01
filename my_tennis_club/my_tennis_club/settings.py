ba050439a2e9e5c286e1ca266adb96f5e67ce59c
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2024-03-01 13:12:53 +0100

    Add old base as comment

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -75,6 +75,10 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tennis_club_database',
