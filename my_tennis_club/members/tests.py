1c14865fd44a1c279e6457b141a6fc4bb3d86b65
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2024-03-04 18:23:37 +0100

    Return Json as response instead of String

--- my_tennis_club/members/tests.py
+++ my_tennis_club/members/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -5,19 +7,21 @@ from .models import Member
 
 
 def create_member(name, surname, email, mobile_phone):
+    return Member.objects.create(name=name, surname=surname, email=email, mobile_phone=mobile_phone)
 
-    return Member.objects.create(name=name, surname=surname, email=email , mobile_phone=mobile_phone)
 
 class MembersModelTests(TestCase):
     def test_should_return_empty_response(self):
         response = self.client.get(reverse("members:members"))
         self.assertEqual(response.status_code, 200)
-        # self.assertContains(response, '')
+        self.assertContains(response, '')
 
     def test_should_return_list_of_members(self):
-        member1 = create_member('John', 'Dou', 'john.dou@test.com', '1234567')
-        member2 = create_member('Max', 'Pain', 'max.pain@test.com', '7654321')
+        create_member('John', 'Dou', 'john.dou@test.com', '1234567')
+        create_member('Max', 'Pain', 'max.pain@test.com', '7654321')
+        members = Member.objects.all().values()
+        expected_json = json.dumps(list(members))
 
         response = self.client.get(reverse("members:members"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, str(member1) + str(member2))
+        self.assertContains(response, expected_json)
