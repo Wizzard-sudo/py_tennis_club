aedea694e69cd10e195e371d3810a6db794ffb32
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2024-03-04 17:39:59 +0100

    Add tests to get members endpoint

--- my_tennis_club/members/tests.py
+++ my_tennis_club/members/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Member
+
+
+def create_member(name, surname, email, mobile_phone):
+
+    return Member.objects.create(name=name, surname=surname, email=email , mobile_phone=mobile_phone)
+
+class MembersModelTests(TestCase):
+    def test_should_return_empty_response(self):
+        response = self.client.get(reverse("members:members"))
+        self.assertEqual(response.status_code, 200)
+        # self.assertContains(response, '')
+
+    def test_should_return_list_of_members(self):
+        member1 = create_member('John', 'Dou', 'john.dou@test.com', '1234567')
+        member2 = create_member('Max', 'Pain', 'max.pain@test.com', '7654321')
+
+        response = self.client.get(reverse("members:members"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, str(member1) + str(member2))
