66fd20b95e8f7a785c047681a1b56830f8aaae0d
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2024-03-20 16:55:28 +0100

    Add endpoint to update member

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.MemberListView.as_view(), name="members-list"),
     path("<int:id>/", views.get_member_by_id, name="member-get"),
     path("create/", views.MemberCreateView.as_view(), name="members-save"),
+    path("update/<pk>", views.MemberUpdateView.as_view(), name="members-update"),
 ]
